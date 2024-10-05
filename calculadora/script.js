982ed3dda1bb23d50f7c503bdc99ac31c2bb1fca
LucayanFelipe <161391775+LucayanFelipe@users.noreply.github.com>
2024-10-05 01:04:15 -0400

    Add files via upload

---
+++ calculadora/script.js
@@ -0,0 +1,13 @@
+
+
+
+
+
+function teste() {
+
+    var valor1 = Number(document.getElementById("v1").value);
+    var valor2 = Number(document.getElementById("v2").value);
+    var resultado = valor1+valor2;
+    alert(resultado);
+    //document.getElementById("resultado").textContent = resultado;
+}
